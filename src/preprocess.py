28b65ca4225e590f8be97438671c1b754b57359b
Pierre Epron <pierre.epron@gmail.com>
2023-06-18 12:37:07 +0200

    add egalite_distrib

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,23 @@
+from pathlib import Path
 import re
-from functools import reduce
 
-PATTERNS = [
-    lambda x: re.sub(r'”', r'"', x),
-    lambda x: re.sub(r'’', r"'", x),
-    lambda x: re.sub(r'([A-Za-z])-[\s\n]+([A-Za-z])', r'\1\2', x)
-]
+from bs4 import BeautifulSoup
+import pandas as pd
 
-def fix_text(text):
-    return reduce(lambda a, b: b(a), PATTERNS, text)
+from tools.fill_template import normalize_text
+
+PE_PATTERN = re.compile(r"principe d('|e l')[eé]galit[eé]", re.I)
+
+def get_pe_paragraphs(path):
+    paragraphs = []
+
+    for path in list(path.glob('*.xml')):
+        soup = BeautifulSoup(normalize_text(path.read_text(encoding='utf-8')), features='xml')
+        for p in soup.find_all('p'):
+            paragraphs.append((path.stem, p.text.strip()))
+
+    paragraphs = pd.DataFrame(paragraphs, columns=['fname', 'text'])
+    paragraphs['is_pe'] = paragraphs['text'].apply(lambda x: True if PE_PATTERN.search(x) else False)
+    paragraphs['year'] = paragraphs['fname'].apply(lambda x: x[2:6])
+
+    return paragraphs
